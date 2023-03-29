d3ad8095587da16563f860779e09e1271e7dddac
shivam <lit2019064@iiitl.ac.in>
2023-03-29 14:34:45 +0530

    breadCrumb

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React from "react";
+import CreateBreadCrumb from "./components/breadCrumbList";
 
 function App() {
-  return <div className="container"></div>;
+  return <div className="container">
+  <CreateBreadCrumb/>
+  </div>;
 }
 
 export default App;
