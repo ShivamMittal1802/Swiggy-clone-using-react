614ca983e7f4a114af1907dd512373c9fb672633
mayank <250900mj@gmail.com>
2023-03-29 15:28:34 +0530

    created footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
 import CreateBreadCrumb from "./components/breadCrumbList";
+import Footer from "./components/footer";
 
 function App() {
-  return <div className="container">
-  <CreateBreadCrumb/>
-  </div>;
+  return (
+    <div className="container">
+      <CreateBreadCrumb />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/footer/index.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div id="swiggy">
+        <img
+          src="https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_284/Logo_f5xzza"
+          alt=""
+        />
+      </div>
+      <div id="swiggy-2023">2023 Swiggy</div>
+      <div id="socials">
+        <img
+          src="https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_48,h_48/icon-facebook_tfqsuc"
+          alt="facebook-logo"
+        />
+        <img
+          src="https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_48,h_48/icon-pinterest_kmz2wd"
+          alt="pinterest-logo"
+        />
+        <img
+          src="https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_48,h_48/icon-instagram_b7nubh"
+          alt="instagram-logo"
+        />
+        <img
+          src="https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_48,h_48/icon-twitter_gtq8dv"
+          alt="twitter-logo"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
