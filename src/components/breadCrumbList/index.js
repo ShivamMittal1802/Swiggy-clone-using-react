44c7efc6f3dd1dc683957db87d080026f6076e1b
itsakarya <aniket2018pr@gmail.com>
2023-04-04 14:18:59 +0530

    modify Breadcrumb

--- src/components/breadCrumbList/index.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function BreadCrumb() {
-  return (
-    <div className="pageLocation">
-      <span>Home/Bangalore/Marathahalli/Meghna Foods</span>
-    </div>
-  );
-}
